63c0c5f0b707930029739eddb71e40a42ed92658
admin <travers-2010@mail.ru>
2024-03-13 16:14:03 +0700

    AOP annotation

--- src/main/java/com/example/rest/rest/mapper/v2/ClientMapperV2.java
+++ src/main/java/com/example/rest/rest/mapper/v2/ClientMapperV2.java
@@ -9,7 +9,6 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE,uses = {OrderMapperV2.class})
